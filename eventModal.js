013fc7ffefe2b68ba60d13c62e6a67dbed5612f4
Emilija <emilijar@wix.com>
2023-08-22 16:36:44 +0300

    Passing root into constructor and setting default values to a modal

--- eventModal.js
+++ eventModal.js
@@ -2,29 +2,35 @@ import { Event } from "./event.js";
 
 export class EventModal {
   constructor(root) {
-    this.startTime = document.querySelector(".start-time");
-    this.endTime = document.querySelector(".end-time");
-    this.eventTitle = document.querySelector(".form-body__add-item");
-    this.modalContainer = document.querySelector(".event-modal");
-    this.saveBtn = document
-      .querySelector(".save")
-      .addEventListener("click", () => {
-        this.onSaveFn(
-          new Event(
-            this.eventTitle.value,
-            new Date(this.startTime.value),
-            new Date(this.endTime.value)
-          )
-        );
-      });
+    this.root = root;
+    this.saveBtn = root.querySelector(".save");
+    this.startTime = root.querySelector(".start-time");
+    this.endTime = root.querySelector(".end-time");
+    this.eventTitle = root.querySelector(".form-body__add-item");
+    this.saveBtn.addEventListener("click", () => {
+      this.onSaveFn(
+        new Event(
+          this.eventTitle.value,
+          new Date(this.startTime.value),
+          new Date(this.endTime.value)
+        )
+      );
+    });
   }
 
   open() {
-    this.modalContainer.style.display = "flex";
+    this.root.style.display = "flex";
+    const currenTime = new Date();
+    currenTime.setMinutes(
+      currenTime.getMinutes() - currenTime.getTimezoneOffset()
+    );
+    this.eventTitle.value = "";
+    this.startTime.value = currenTime.toISOString().slice(0, 16);
+    this.endTime.value = currenTime.toISOString().slice(0, 16);
   }
 
   close() {
-    this.modalContainer.style.display = "none";
+    this.root.style.display = "none";
   }
 
   onSave(onSaveFn) {
