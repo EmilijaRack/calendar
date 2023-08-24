be678d498a71d83e553a4796921158ab5af863ff
Emilija <emilijar@wix.com>
2023-08-24 12:46:17 +0300

    creating calcEventLength function

--- renderer.js
+++ renderer.js
@@ -5,6 +5,7 @@ const DAYS_IN_A_WEEK = 7;
 
 export class Renderer {
   constructor(root) {
+    this.eventDataContainer = document.createElement("div");
     this.calendarCells = root.querySelectorAll(".main-calendar__body__cells");
     this.cellArray = new Array(HOURS_IN_A_DAY)
       .fill(0)
@@ -15,10 +16,10 @@ export class Renderer {
       }
     }
   }
+
   renderEvent(event) {
-    const eventDataContainer = document.createElement("div");
-    eventDataContainer.setAttribute("class", "event");
-    eventDataContainer.innerText = `${
+    this.eventDataContainer.setAttribute("class", "event");
+    this.eventDataContainer.innerText = `${
       event.title
     }, ${event.startDate.getHours()}:${
       event.startDate.getMinutes() < 10
@@ -31,32 +32,38 @@ export class Renderer {
     }`;
     this.cellArray[event.startDate.getHours()][
       event.startDate.getDay()
-    ].appendChild(eventDataContainer);
+    ].appendChild(this.eventDataContainer);
+    this.calcEventLength(event);
+  }
 
+  calcEventLength(event) {
     const eventLength = (event.endDate - event.startDate) / (1000 * 60 * 60);
     const dayFitLength = HOURS_IN_A_DAY - event.startDate.getHours();
     const offSet = (CELL_HEIGHT * event.startDate.getMinutes()) / 60;
     const fullEventHeight = CELL_HEIGHT * eventLength;
-    eventDataContainer.style.top = offSet + "px";
+    const eventDaysLength = Math.floor(eventLength / HOURS_IN_A_DAY);
+    const previousHeight =
+      (dayFitLength * FULL_DAY_LENGTH) / HOURS_IN_A_DAY - offSet;
+
+    this.eventDataContainer.style.top = offSet + "px";
+
     if (eventLength <= dayFitLength) {
-      eventDataContainer.style.height = fullEventHeight + "px";
-    } else {
-      let previousHeight =
-        (dayFitLength * FULL_DAY_LENGTH) / HOURS_IN_A_DAY - offSet;
-      eventDataContainer.style.height = previousHeight + "px";
-      for (let i = 1; i <= Math.floor(eventLength / HOURS_IN_A_DAY); i++) {
-        const eventDataContainerNextDay = document.createElement("div");
-        eventDataContainerNextDay.setAttribute("class", "event");
-        this.cellArray[0][event.startDate.getDay() + i].appendChild(
-          eventDataContainerNextDay
-        );
-        if (i === Math.floor(eventLength / HOURS_IN_A_DAY)) {
-          eventDataContainerNextDay.style.height =
-            FULL_DAY_LENGTH - previousHeight + offSet + "px";
-        } else {
-          eventDataContainerNextDay.style.height = FULL_DAY_LENGTH + "px";
-        }
+      this.eventDataContainer.style.height = fullEventHeight + "px";
+      return;
+    }
+    this.eventDataContainer.style.height = previousHeight + "px";
+    for (let i = 1; i <= eventDaysLength; i++) {
+      const eventDataContainerNextDay = document.createElement("div");
+      eventDataContainerNextDay.setAttribute("class", "event");
+      this.cellArray[0][event.startDate.getDay() + i].appendChild(
+        eventDataContainerNextDay
+      );
+      if (i === eventDaysLength) {
+        eventDataContainerNextDay.style.height =
+          FULL_DAY_LENGTH - previousHeight + offSet + "px";
+        return;
       }
+      eventDataContainerNextDay.style.height = FULL_DAY_LENGTH + "px";
     }
   }
 }
