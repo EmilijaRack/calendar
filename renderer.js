97441197ea5515d4370753ac32908fd99a8c543e
Emilija <emilijar@wix.com>
2023-08-24 09:40:25 +0300

    Rendering events that last a few days

--- renderer.js
+++ renderer.js
@@ -1,12 +1,17 @@
 const CELL_HEIGHT = 50;
+const FULL_DAY_LENGTH = 1200;
+const HOURS_IN_A_DAY = 24;
+const DAYS_IN_A_WEEK = 7;
 
 export class Renderer {
   constructor(root) {
     this.calendarCells = root.querySelectorAll(".main-calendar__body__cells");
-    this.cellArray = new Array(24).fill(0).map(() => new Array(7));
-    for (let i = 0; i < 24; i++) {
-      for (let j = 0; j < 7; j++) {
-        this.cellArray[i][j] = this.calendarCells[i * 7 + j];
+    this.cellArray = new Array(HOURS_IN_A_DAY)
+      .fill(0)
+      .map(() => new Array(DAYS_IN_A_WEEK));
+    for (let i = 0; i < HOURS_IN_A_DAY; i++) {
+      for (let j = 0; j < DAYS_IN_A_WEEK; j++) {
+        this.cellArray[i][j] = this.calendarCells[i * DAYS_IN_A_WEEK + j];
       }
     }
   }
@@ -27,9 +32,31 @@ export class Renderer {
     this.cellArray[event.startDate.getHours()][
       event.startDate.getDay()
     ].appendChild(eventDataContainer);
+
     const eventLength = (event.endDate - event.startDate) / (1000 * 60 * 60);
-    eventDataContainer.style.height = CELL_HEIGHT * eventLength + "px";
-    eventDataContainer.style.top =
-      (CELL_HEIGHT * event.startDate.getMinutes()) / 60 + "px";
+    const dayFitLength = HOURS_IN_A_DAY - event.startDate.getHours();
+    const offSet = (CELL_HEIGHT * event.startDate.getMinutes()) / 60;
+    const fullEventHeight = CELL_HEIGHT * eventLength;
+    eventDataContainer.style.top = offSet + "px";
+    if (eventLength <= dayFitLength) {
+      eventDataContainer.style.height = fullEventHeight + "px";
+    } else {
+      let previousHeight =
+        (dayFitLength * FULL_DAY_LENGTH) / HOURS_IN_A_DAY - offSet;
+      eventDataContainer.style.height = previousHeight + "px";
+      for (let i = 1; i <= Math.floor(eventLength / HOURS_IN_A_DAY); i++) {
+        const eventDataContainerNextDay = document.createElement("div");
+        eventDataContainerNextDay.setAttribute("class", "event");
+        this.cellArray[0][event.startDate.getDay() + i].appendChild(
+          eventDataContainerNextDay
+        );
+        if (i === Math.floor(eventLength / HOURS_IN_A_DAY)) {
+          eventDataContainerNextDay.style.height =
+            FULL_DAY_LENGTH - previousHeight + offSet + "px";
+        } else {
+          eventDataContainerNextDay.style.height = FULL_DAY_LENGTH + "px";
+        }
+      }
+    }
   }
 }
