4c417c0bf1bec3ee03382e44e357aa40f9ad6c9d
Emilija <emilijar@wix.com>
2023-09-12 09:09:23 +0300

    adding more methods to mainCalendarState

--- mainCalendarState.js
+++ mainCalendarState.js
@@ -1,5 +1,26 @@
 export class MainCalendarState {
-  constructor(displayDate) {
+  constructor(displayDate, events) {
     this.displayDate = displayDate ?? new Date();
+    this.events = events ?? [];
+  }
+
+  updateEvents(events) {
+    this.events = events;
+  }
+
+  addEvent(event) {
+    this.events = [...this.events, event];
+  }
+
+  removeEvent(id) {
+    this.events = this.events.filter((event) => event.id !== id);
+  }
+
+  addDisplayWeekOffset(offset) {
+    this.displayDate = new Date(
+      this.displayDate.getFullYear(),
+      this.displayDate.getMonth(),
+      this.displayDate.getDate() + offset
+    );
   }
 }
